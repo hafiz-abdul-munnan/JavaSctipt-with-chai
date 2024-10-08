95b93d0a957c34933a541cc2451cb1c410583b57
Hafiz Abdul Munnan <56857240+hafiz-abdul-munnan@users.noreply.github.com>
2024-10-08 08:48:28 +0000

    datatypes learning

---
+++ 01_js_basic/02_datatypes.js
@@ -0,0 +1,23 @@
+"use strict"; // treat all JS code as newer version
+
+//alert(3+3); // we are using nodejs, not browser
+let uName = "Hafiz";
+let age = 25;
+let isloggedIn = false;
+
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value 
+// undifined => 
+// symbol 
+
+
+//object
+
+console.log(typeof uName)
+console.log(typeof undefined); // type i undefined
+console.log(typeof null); // type is object
+
