e25b3b458117cf42f5f7e37b633e0d00185c9df8
Hafiz Abdul Munnan <56857240+hafiz-abdul-munnan@users.noreply.github.com>
2024-10-08 08:05:46 +0000

    create files and ;earn about variables

---
+++ 01_js_basic/01_variables.js
@@ -0,0 +1,12 @@
+const accountID = 123456789;
+let accountEmail = "hafiz.abdul.munnan@gmail.com";
+var accountPassword = "123789456";
+accountCity = "Faisalabad";
+
+
+/*
+prefer not to use var
+because of issue in block scope and functional scope
+*/
+
+console.table([accountID, accountEmail, accountPassword, accountCity])
