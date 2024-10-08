56fa1d3c7a1fdc125a7516b6faf69f84417404af
Hafiz Abdul Munnan <56857240+hafiz-abdul-munnan@users.noreply.github.com>
2024-10-08 09:26:28 +0000

    Conversion Of Datatypes

---
+++ 01_js_basic/03_conversionOperation.js
@@ -0,0 +1,13 @@
+// let score = '55abc'; // Type is NaN
+let score = null; // JS me type 0 say start hota hen
+// let score = true; // type iska 1 milta 
+// let score = undefined; //Type is NaN
+
+// console.log(typeof score);
+// console.log(typeof (score));
+
+let valueInNumber = Number(score); // Test to the Number
+// let valueInNumber = Boolean(score); // Test to the true or false 
+// let valueInNumber = String(score); // Test to the any value of change to string 
+console.log(typeof(valueInNumber));
+console.log(valueInNumber);
