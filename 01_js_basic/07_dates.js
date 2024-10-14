80085261e48bd6af1a3fedac72caf38e376e12b1
Hafiz Abdul Munnan <56857240+hafiz-abdul-munnan@users.noreply.github.com>
2024-10-14 09:27:01 +0000

    Dates and Arrays

---
+++ 01_js_basic/07_dates.js
@@ -0,0 +1,23 @@
+const myDate = new Date();
+
+// console.log(myDate);
+// console.log(myDate.toString());
+
+// JS me month 0 say start hoty hen 
+// console.log(myDate.toDateString());
+
+// console.log(myDate.toISOString());
+// console.log(myDate.toJSON());
+// console.log(myDate.toLocaleDateString());
+// console.log(myDate.toLocaleString());
+// console.log(myDate.toLocaleTimeString());
+// console.log(myDate.toTimeString());
+// console.log(typeof myDate);
+
+console.log(myDate.getDay());
+console.log(myDate.getFullYear());
+console.log(myDate.getHours());
+console.log(myDate.getMonth());
+console.log(myDate.getMinutes());
+console.log(myDate.getTime());
+
