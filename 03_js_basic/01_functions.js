a62ed724b5baab6210e6e422ed0808da4111ba3c
Hafiz Abdul Munnan <56857240+hafiz-abdul-munnan@users.noreply.github.com>
2024-10-17 05:30:35 +0000

    Functions Learning

---
+++ 03_js_basic/01_functions.js
@@ -0,0 +1,42 @@
+// function sayMyName() {
+//     console.log("H");
+//     console.log("A");
+//     console.log("F");
+//     console.log("I");
+//     console.log("Z");
+//     console.log(" ");
+//     console.log("A");
+//     console.log("B");
+//     console.log("D");
+//     console.log("U");
+//     console.log("L");
+//     console.log(" ");
+//     console.log("M");
+//     console.log("U");
+//     console.log("N");
+//     console.log("N");
+//     console.log("A");
+//     console.log("N");
+// }
+// sayMyName()
+
+
+
+// function addTwoNumbers(num1, num2) {
+//     console.log(num1 + num2);
+// }
+
+
+
+// function addTwoNumbers(num1, num2) {
+    // Return kay bad ka code kabi be nai chalta is leeian koi aaisa code ho jo pehly chalana ho to ussy return say pehly run karain 
+//     return num1 + num2
+// }
+// const result = addTwoNumbers(10, 20);
+// console.log("Result is: ", result);
+
+
+function userLogedIn(user) {
+    return `${user} is Logged in Right now`
+}
+console.log(userLogedIn("Hafiz Abdul Munnan"));
